9e2183d21d4e2f23d379d8d9e3e104cd33631ecf
meliktn <36202241+meliktn@users.noreply.github.com>
2018-07-05 12:23:11 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+ app.get('/dateValues/:dateVal', function(req,res,next){
+     var dateVal = req.params.dateVal;
+
+    var dateFormattingOptions = { 
+        year: 'numeric',
+        month:'long',
+        day: 'numeric'
+    };
+ if (isNaN(dateVal)){
+     var naturalDate = new Date(dateVal);
+     naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+     var unixDate = new Date(dateVal).getTime()/1000;}
+     else { var unixDate = dateVal;
+    var naturalDate = new Date(dateVal *1000);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);}
+
+
+     res.json({unix: dateVal, natural: naturalDate});
+    
+   
+ });
+
+app.listen(3000, function(){
+    console.log('working');
+});
